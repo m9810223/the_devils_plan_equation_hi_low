f4761a72a51bfd05b6d64539c62fc3c72d870c88
Michael <m9810223@gmail.com>
2024-01-31 00:23:46 +0800

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,28 @@ type DiffCalcResultProps = {dcrs: DiffCalcResultT}
 
 function DiffCalcResult({dcrs}: DiffCalcResultProps) {
     return dcrs.map(([d, cr]) => (
-        <>
-            <p key={1}>±{d}</p>
+        <div key={d}>
+            <p>±{d}</p>
             {cr.map(([c, r]) => (
                 <p key={c}>
                     {c} = {r}
                 </p>
             ))}
             <br />
-        </>
+        </div>
     ))
 }
 
 function Main() {
+    // py
     const {runPython, stdout, stderr, isLoading, isRunning} = usePython()
     const [mainCode, setMainCode] = useState({})
     // ref
     const numsRef = useRef<HTMLInputElement>(null)
     const signRef = useRef<HTMLInputElement>(null)
     const sqrtRef = useRef<HTMLInputElement>(null)
+    const mRef = useRef<HTMLInputElement>(null)
+    const nRef = useRef<HTMLInputElement>(null)
     // init
     useEffect(() => {
         fetch('/the_devils_plan_equation_hi_low/main.py')
@@ -42,8 +45,10 @@ function Main() {
         const sign = JSON.stringify('/' + signRef?.current?.value.trim())
         const sqrt = JSON.stringify(JSON.stringify(sqrtRef?.current?.checked))
         const nums = JSON.stringify(numsRef?.current?.value.trim())
-        console.log({sign, sqrt, nums})
-        const code = `js_api(${sign}, ${sqrt}, ${nums}, 5, 2)`
+        const m = JSON.stringify(mRef?.current?.value.trim())
+        const n = JSON.stringify(nRef?.current?.value.trim())
+        console.log({sign, sqrt, nums, m, n})
+        const code = `js_api(${sign}, ${sqrt}, ${nums}, ${m}, ${n})`
         console.log('code', code)
         runPython(mainCode + code)
     }
@@ -51,14 +56,21 @@ function Main() {
     return (
         <>
             <p>
-                符號： {`'/'`} and <input ref={signRef} defaultValue={'+*'} placeholder="*+- 三選二" />
+                符號： {`'/'`} and <input ref={signRef} defaultValue="+*" placeholder="*+- 三選二" />
             </p>
             <p>
                 根號： <input type="checkbox" ref={sqrtRef} defaultChecked={false} />
             </p>
             <p>
-                數字： <input ref={numsRef} defaultValue={'0 1 2 10'} placeholder="0 ~ 10 選四，空白分隔" />
+                數字： <input ref={numsRef} defaultValue="0 1 2 10" placeholder="0 ~ 10 選四，空白分隔" />
             </p>
+            <p>
+                m: <input ref={mRef} defaultValue="5" />
+            </p>
+            <p>
+                n: <input ref={nRef} defaultValue="1" />
+            </p>
+
             <input
                 type="button"
                 value={isRunning ? 'running...' : 'run'}
